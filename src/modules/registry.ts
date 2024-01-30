a1a2b5de69f2a5b293da6ce4b9196ce1e3de332a
Tkd-Alex <alex.tkd.alex@gmail.com>
2024-01-30 17:17:51 +0100

    Fix registry import, after copy & paste

--- src/modules/registry.ts
+++ src/modules/registry.ts
@@ -3,9 +3,9 @@ import { defaultRegistryTypes } from "@cosmjs/stargate";
 
 import { registry as NodeRegistry } from "./node/registry";
 import { registry as PlanRegistry } from "./plan/registry";
-import { registry as ProviderRegistry } from "./plan/registry";
-import { registry as SessionRegistry } from "./plan/registry";
-import { registry as SubscriptionRegistry } from "./plan/registry";
+import { registry as ProviderRegistry } from "./provider/registry";
+import { registry as SessionRegistry } from "./session/registry";
+import { registry as SubscriptionRegistry } from "./subscription/registry";
 
 export const SentinelRegistry: ReadonlyArray<[string, GeneratedType]> = [
     ...defaultRegistryTypes,
