2fe15ecd096219cf733921163da6d16d96f4efb5
Tkd-Alex <alex.tkd.alex@gmail.com>
2024-02-09 16:32:01 +0100

    Export bw, status and pagination to sentinel-sdk/types

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+import { Bandwidth } from "./protobuf/sentinel/types/v1/bandwidth";
+import { Status } from "./protobuf/sentinel/types/v1/status";
+import { PageRequest } from "./protobuf/cosmos/base/query/v1beta1/pagination";
+
+export { Bandwidth };
+export { Status };
+export { PageRequest };
