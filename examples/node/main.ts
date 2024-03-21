951f741337d8a5ffe1af74ecfd26919d1b5ec3c9
Tkd-Alex <alex.tkd.alex@gmail.com>
2024-03-21 10:27:47 +0100

    Full implementations of https://github.com/sentinel-official/cli-client/blob/master/cmd/connect.go in examples/node folder (one v2ray and one wg)

--- examples/node/main.ts
+++ examples/node/main.ts
@@ -1,31 +1,54 @@
-import { SentinelClient, SigningSentinelClient } from "@sentinel-official/sentinel-js-sdk";
-import { Status, PageRequest } from "@sentinel-official/sentinel-js-sdk";
-import { TxNodeSubscribe, nodeSubscribe } from "@sentinel-official/sentinel-js-sdk";
-import { searchEvent } from "@sentinel-official/sentinel-js-sdk";
+// Re-implementation of: https://github.com/sentinel-official/cli-client/blob/master/cmd/connect.go
 
-import { NodeEventCreateSubscription, isNodeEventCreateSubscription } from "@sentinel-official/sentinel-js-sdk";
+import {
+    Node,
+    NodeStatus,
+    SentinelClient,
+    SessionEventStart,
+    SigningSentinelClient,
+    V2Ray,
+    Wireguard,
+    postSession,
+    signSessionId,
+    Status,
+    PageRequest,
+    TxNodeSubscribe,
+    nodeSubscribe,
+    TxSessionStart,
+    sessionStart,
+    searchEvent,
+    nodeStatus,
+    NodeEventCreateSubscription
+} from "@sentinel-official/sentinel-js-sdk";
+
 
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
-import { GasPrice } from "@cosmjs/stargate"
+import { GasPrice, assertIsDeliverTxSuccess } from "@cosmjs/stargate"
+import { Bip39, Slip10Curve, EnglishMnemonic, Slip10 } from "@cosmjs/crypto"
+import { makeCosmoshubPath } from "@cosmjs/amino"
 
 import Long from "long";
 
 import * as readline from 'readline/promises';
 
-const queryNodes = async () => {
+const queryNodes = async (limit: number) => {
     const rpc = "https://rpc.sentinel.co:443"
     const client = await SentinelClient.connect(rpc)
     const nodes = await client.sentinelQuery?.node.nodes(
         Status.STATUS_ACTIVE,
-        PageRequest.fromPartial({
-            limit: 5,
-            countTotal: true
-        })
+        PageRequest.fromPartial({ limit: limit, countTotal: true })
     )
     return nodes
 }
 
-const subscribeToNode = async (sentnode: string, gygabyte: number, denom: string) => {
+const queryNode = async (sentnode: string) => {
+    const rpc = "https://rpc.sentinel.co:443"
+    const client = await SentinelClient.connect(rpc)
+    const node = await client.sentinelQuery?.node.node(sentnode)
+    return node
+}
+
+const main = async () => {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
@@ -36,6 +59,12 @@ const subscribeToNode = async (sentnode: string, gygabyte: number, denom: string
 
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: "sent" });
     const [account] = await wallet.getAccounts();
+    console.log(`Wallet: ${account.address}`)
+
+    // Export private key in order to signSessionId
+    const seed = await Bip39.mnemonicToSeed(new EnglishMnemonic(wallet.mnemonic), "");
+    const { privkey } = Slip10.derivePath(Slip10Curve.Secp256k1, seed, makeCosmoshubPath(0));
+    // console.log(privkey)
 
     const customDenom = "udvpn"
     const gasPrice = GasPrice.fromString(`0.2${customDenom}`);
@@ -45,36 +74,99 @@ const subscribeToNode = async (sentnode: string, gygabyte: number, denom: string
         gasPrice: gasPrice
     })
 
-    console.log(account.address)
-    const args: TxNodeSubscribe = {
-        from: account.address,
-        nodeAddress: sentnode,
-        gigabytes: Long.fromNumber(gygabyte, true),
-        denom,  // same name
-    }
-    const msg = nodeSubscribe(args)
-    console.log(msg)
+    var selectedNodes: {"node": Node, "status": NodeStatus}[] = []
+    var v2Node: {"node": Node, "status": NodeStatus} | null = null;
+    var wgNode: {"node": Node, "status": NodeStatus} | null = null;
 
-    const tx = await client.signAndBroadcast(account.address, [msg], "auto", "")
-    console.log(tx)
-
-    const eventCreateSubscription = searchEvent(NodeEventCreateSubscription.type, tx.events);
-    if(eventCreateSubscription) {
-        console.log("isSubscriptionEventAllocate", isNodeEventCreateSubscription(eventCreateSubscription))
-        const eventParsed = NodeEventCreateSubscription.parse(eventCreateSubscription)
-        console.log(eventParsed)
-        console.log(`Your subscription id is: ${eventParsed.value.id}`)
-    } else console.log("eventCreateSubscription, not founded")
-}
-
-
-const main = async () => {
-    const nodes = await queryNodes()
-    console.log(nodes)
+    const nodes = await queryNodes(500);
     if (nodes?.nodes){
-        const random = Math.floor(Math.random() * nodes?.nodes.length);
-        const node = nodes?.nodes[random];
-        subscribeToNode(node.address, 1, "udvpn")
+        // Search for a v2ray node and a wireguard node please.
+        for(var i=0; i<nodes.nodes.length; i++){
+            try {
+                var status = await nodeStatus(nodes.nodes[i].remoteUrl)
+                console.log(status)
+                if (status.type == 1) wgNode = { node: nodes.nodes[i], status }
+                else if(status.type == 2) v2Node = { node: nodes.nodes[i], status }
+                if (v2Node !== null && wgNode !== null) break // both nodes was founded
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
+
+    console.log("\n\n")
+
+    if(v2Node !== null && wgNode !== null){
+        selectedNodes.push(v2Node);
+        selectedNodes.push(wgNode);
+
+        for(const node of selectedNodes){
+            var subscribeArgs: TxNodeSubscribe = {
+                from: account.address,
+                nodeAddress: node.node.address,
+                gigabytes: Long.fromNumber(1, true),
+                denom: "udvpn",  // same name
+            }
+            var subscribeTx = await client.signAndBroadcast(account.address, [nodeSubscribe(subscribeArgs)], "auto", "sentinel-js-sdk")
+            assertIsDeliverTxSuccess(subscribeTx)
+            var eventCreateSubscription = searchEvent(NodeEventCreateSubscription.type, subscribeTx.events);
+            if(eventCreateSubscription) {
+
+                // console.log("isSubscriptionEventAllocate", isNodeEventCreateSubscription(eventCreateSubscription))
+                var subscriptionEventParsed = NodeEventCreateSubscription.parse(eventCreateSubscription)
+                console.log("Event parsed", subscriptionEventParsed)
+                var subscriptionId = subscriptionEventParsed.value.id
+                console.log(`Your subscription id is: ${subscriptionId}`)
+
+                var sessionStartArgs:  TxSessionStart = {
+                    from: account.address,
+                    id: subscriptionId,
+                    address: node.node.address
+                }
+                var sessionStartTx = await client.signAndBroadcast(account.address, [sessionStart(sessionStartArgs)], "auto", "sentinel-js-sdk")
+                assertIsDeliverTxSuccess(sessionStartTx)
+
+                var eventSessionStart = searchEvent(SessionEventStart.type, sessionStartTx.events);
+                if(eventSessionStart){
+                    var sessionStartEventParsed = SessionEventStart.parse(eventSessionStart)
+                    console.log("Event parsed", sessionStartEventParsed)
+                    var sessionId = sessionStartEventParsed.value.id
+                    console.log(`Your session id is: ${sessionId}`)
+
+                    await new Promise(f => setTimeout(f, 10000));
+                    // wait for rpc sync... else 'session 123456789 does not exist'
+
+                    // make sure the session exist on chain, let's query the chain
+                    var session = await client.sentinelQuery?.session.session(sessionId)
+                    console.log("Session on chain: ", session)
+
+                    var signature = signSessionId(privkey, sessionId)
+
+                    if(node.status.type == 1){
+                        console.log("Wireguard")
+                        var wgConfig = new Wireguard()
+                        var wgPostResponse = await postSession(wgConfig.publicKey, signature, account.address, sessionId, node.node.remoteUrl)
+                        console.log(wgPostResponse)
+                        if (wgPostResponse.success === true){
+                            await wgConfig.parseConfig(wgPostResponse.result as string)
+                            wgConfig.writeConfig("wg0.conf")
+                        }
+                    }
+                    else if(node.status.type == 2){
+                        console.log("V2Ray")
+                        var v2Config = new V2Ray()
+                        var v2PostResponse = await postSession(v2Config.getKey(), signature, account.address, sessionId, node.node.remoteUrl)
+                        console.log(v2PostResponse)
+                        if (v2PostResponse.success === true){
+                            await v2Config.parseConfig(v2PostResponse.result as string)
+                            v2Config.writeConfig("v2ray_config.json")
+                        }
+                    }
+
+                } else console.log("eventSessionStart, not founded")
+            } else console.log("eventCreateSubscription, not founded")
+
+        }
     }
 }
 
