6085ff791a01dd96834ce23a51e8190b71e50c3f
Tkd-Alex <alex.tkd.alex@gmail.com>
2026-03-27 16:34:18 +0100

    downgrade to ts-proto@1.180.0 (CosmJS use the old Writer instead of BinaryWriter from 2x of ts-proto)

--- scripts/install-protoc.sh
+++ scripts/install-protoc.sh
@@ -9,4 +9,4 @@ curl -L https://github.com/protocolbuffers/protobuf/releases/download/v25.9/prot
 unzip -o protoc.zip
 rm protoc.zip
 ln -s /usr/lib/protoc/bin/protoc /usr/local/bin/protoc
-npm install ts-proto@2.11.6 --save-dev
+npm install ts-proto@1.180.0 --save-dev
